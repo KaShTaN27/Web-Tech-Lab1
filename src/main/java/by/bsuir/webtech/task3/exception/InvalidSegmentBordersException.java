1042fee4dc287c0e54ea0e369386fd2498a2691b
KaShTaN27 <dkachan.lida@mail.ru>
2022-09-19 21:20:14 +0300

    Add task 3 custom exceptions

---
+++ src/main/java/by/bsuir/webtech/task3/exception/InvalidSegmentBordersException.java
@@ -0,0 +1,7 @@
+package by.bsuir.webtech.task3.exception;
+
+public class InvalidSegmentBordersException extends RuntimeException{
+    public InvalidSegmentBordersException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/by/bsuir/webtech/task3/exception/InvalidSegmentStepException.java
@@ -0,0 +1,7 @@
+package by.bsuir.webtech.task3.exception;
+
+public class InvalidSegmentStepException extends RuntimeException{
+    public InvalidSegmentStepException(String message) {
+        super(message);
+    }
+}
