1de211918c99fd6dfe3a5367a08523eb060d0b0f
KaShTaN27 <dkachan.lida@mail.ru>
2022-09-19 21:17:05 +0300

    Add task 2 models

---
+++ src/main/java/by/bsuir/webtech/task2/model/Rectangle.java
@@ -0,0 +1,20 @@
+package by.bsuir.webtech.task2.model;
+
+public class Rectangle {
+
+    private final Point topLeft;
+    private final Point bottomRight;
+
+    public Rectangle(Point topLeft, Point bottomRight) {
+        this.topLeft = topLeft;
+        this.bottomRight = bottomRight;
+    }
+
+    public Point getTopLeft() {
+        return topLeft;
+    }
+
+    public Point getBottomRight() {
+        return bottomRight;
+    }
+}
